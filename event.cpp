ab72925cdb21e47d0ac79d3f3f7f66d44611997c
sathes39 <ben.sathes.wardrop@gmail.com>
2015-10-18 13:57:23 +1100

    Completion and bug fixes

--- event.cpp
+++ event.cpp
@@ -1,15 +1,72 @@
 /**************************************************************************************************************
  *	Ben Wardrop 4413234
- *	CSCI262 Assignment 3 
+ *	CSCI262 Assignment 3
  *	Event.h
  *	last Modified: Ben 12/10/15
  **************************************************************************************************************/
 
 #include "event.h"
 
+
+
+
+std::ostream & operator << (std::ostream & os, Event & e)
+{
+	os << e.name << ':';
+	switch(e.cde)
+	{
+		case Event::C:
+			os << 'C';
+			break;
+		case Event::D:
+			os << 'D';
+			break;
+		case Event::E:
+			os << 'E';
+			break;
+		default:
+			os << 'N';
+	}
+	os << ':';
+	os << e.minimum << ':';
+	os << e.maximum << ':';
+	os << e.unit << ':';
+	//os << e.mean << ':';
+	//os << e.stdDev << ':';
+	return os;
+}
+
+std::istream & operator >> (std::istream & in, Event & e)
+{
+	char tempCDE = 'N';
+	getline(in,e.name,':');
+	in >> tempCDE; in.ignore();
+	in >> e.minimum; in.ignore();
+	in >> e.maximum; in.ignore();
+	getline(in,e.unit,':');
+	in >> e.weight; in.ignore();
+	//in >> e.mean; in.ignore();
+	//in >> e.stdDev; in.ignore();
+	switch(tempCDE)
+	{
+		case 'C':
+			 e.cde = Event::C;
+			break;
+		case 'D':
+			e.cde = Event::D;
+			break;
+		case 'E':
+			e.cde = Event::E;
+			break;
+		default:
+			e.cde = Event::N;
+	}
+	return in;
+}
+
 Event::Event()
 {
-	this -> cde		=	static_cast<CDE>(3); // N as part of CDE isn't regognized, what a pain.
+	this -> cde		=	Event::N;
 	this -> minimum =	0;
 	this -> maximum =	0;
 	this -> weight	=	0;
@@ -17,17 +74,21 @@ Event::Event()
 
 Event::Event(string in) //Construct Event from string
 {
+	fromString(in);
+}
+
+void Event::fromString(std::string input)	//Asign values from string
+{
 	std::stringstream ss;
-	ss << in;
-	
-	//working on directing the data from the string stream into the class
+	ss << input;
+	ss >> *(this);
 }
 
-void	fromString(string input)	//Asign values from string
+string toString()					//Create a string representation
 {
-
-}
-string	toString()					//Create a string representation
-{
-
+	Event value;
+	std::stringstream sin;
+	sin << value;
+	string ret = sin.str();
+	return ret;
 }
